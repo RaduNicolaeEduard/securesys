3c16cbce3bf4bf92aeeb00828e56c7a2f394af7b
Radu Nicolae <nicolae.radu99@gmail.com>
2021-10-10 13:25:07 +0300

    update

--- setup.sql
+++ setup.sql
@@ -4,8 +4,8 @@ USE secure;
 
 CREATE TABLE mytable (username VARCHAR(20),email VARCHAR(20),phonenum VARCHAR(20));
 
-INSERT INTO mytable VALUES ('Jhon','Jhon@email,con','00000');
-INSERT INTO mytable VALUES ('Doe','Doe@email,con','11111');
+INSERT INTO mytable VALUES ('Jhon','Jhon@email.com','00000');
+INSERT INTO mytable VALUES ('Doe','Doe@email.com','11111');
 
 CREATE TABLE supersecret (myfield VARCHAR(20));
 
